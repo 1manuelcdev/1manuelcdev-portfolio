99c8d2429e3cb002fc299dd650e346eaa54312d4
Manuel C. <1manuelc.dev@gmail.com>
2026-07-03 22:00:30 -0300

    style: adjust text size and spacing in HeroContent component

--- src/components/fragments/HeroContent.tsx
+++ src/components/fragments/HeroContent.tsx
@@ -4,9 +4,9 @@ import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 export function HeroContent() {
   return (
     <div className="w-full max-w-sm md:max-w-4xl flex flex-col items-center justify-center gap-4">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-3xl md:text-5xl">Design, Engenharia, Atitude</h1>
-        <h2 className="font-normal text-zinc-300 text-base md:text-lg">
+      <div className="flex flex-col items-center gap-2 md:gap-4">
+        <h1 className="text-2xl md:text-5xl">Design, Engenharia, Atitude</h1>
+        <h2 className="font-normal text-zinc-300 text-sm md:text-lg">
           Portfólio em construção, volte em breve...
         </h2>
       </div>
