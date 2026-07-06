4d692469c9354da09c3f2da76c90725dd8a489fd
Manuel C. <1manuelc.dev@gmail.com>
2026-07-06 20:35:57 -0300

    feat(auth): add login form and auth route

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,21 @@
+import { LoginForm } from "@/components/login-form";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+
+      <Image
+        className="blur-sm -z-1 h-full absolute object-cover object-center-left"
+        alt="background hero image"
+        height={1080}
+        width={1920}
+        loading="eager"
+        src={"/frame.jpg"}
+      />
+    </div>
+  );
+}
