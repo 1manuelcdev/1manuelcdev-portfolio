ada04c884c1a79e89fe86d5324c22e418f7beb08
Manuel C. <1manuelc.dev@gmail.com>
2026-07-09 23:10:54 -0300

    fix(images): add remote pattern for avatars.githubusercontent.com

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "eefsvvxawvjdzjdhcbuf.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
 };
